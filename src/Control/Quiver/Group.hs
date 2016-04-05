9122c326c35c1bdb2a8222b77d3f56e7153559ab
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-04-05 10:04:24 +1000

    Initial layout

---
+++ src/Control/Quiver/Group.hs
@@ -0,0 +1,13 @@
+{- |
+   Module      : Control.Quiver.Group
+   Description : Group and chunk values within a Quiver
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : MIT
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+
+
+ -}
+module Control.Quiver.Group where
+
+import Control.Quiver
