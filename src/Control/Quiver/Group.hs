3aeaa4c59c0c82b6b5f136a536ffa17b5c4eab5d
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-04-05 11:07:08 +1000

    The base grouping function.

--- src/Control/Quiver/Group.hs
+++ src/Control/Quiver/Group.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {- |
    Module      : Control.Quiver.Group
    Description : Group and chunk values within a Quiver
@@ -10,4 +11,37 @@
  -}
 module Control.Quiver.Group where
 
-import Control.Quiver
+import Control.Quiver.SP
+
+--------------------------------------------------------------------------------
+
+-- | Accumulate values within a Quiver.
+spaccum :: (a -> p)
+              -- ^ Create the initial partial accumulation @p@.
+           -> (p -> a -> Either p (g, Maybe a))
+              -- ^ Attempt to add a new value to a partial
+              -- accumulation; returns either an updated partial
+              -- accumulation or else a completed accumulation @g@ as
+              -- well as optionally the initial value (if it was /not/
+              -- added to the completed accumulation).
+           -> (p -> Maybe g)
+              -- ^ Attempt to convert the final partial accumulation
+              -- to a complete accumulation.  If this function returns
+              -- @'Nothing'@ then the final partial accumulation is
+              -- returned using 'spfailed'.
+           -> SP a g m p
+spaccum mkInit addA finalise = createNewAccum
+  where
+    createNewAccum = spconsume newAccumFrom spcomplete
+
+    newAccumFrom = accumPartial . mkInit
+
+    accumPartial p = spconsume (withValue . addA p) (finalisePartial p)
+
+    withValue epa = case epa of
+                      Left p        -> accumPartial p
+                      Right (g,ma') -> produce g
+                                               (const (maybe createNewAccum newAccumFrom ma'))
+                                               spincomplete
+
+    finalisePartial p = maybe (spfailed p) spemit (finalise p)
